e91ffef0a1c5b16e9491a309fc5e312c7435c505
Gabrielli Correa <gabriellicorrea7@gmail.com>
2019-08-05 10:51:02 -0300

    Sintaxe Arrow function

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//Sintaxe da Arrow Function
 () => {
 
 }
@@ -5,3 +7,28 @@
 const test = () => {
     console.log("Arrow function.")
 }
+test()
+
+//sem arrow function
+function myFunc(){
+    this.a = 'hello'
+
+    console.log(this.a)
+
+    setInterval(function(){
+        console.log(this.a)
+    }, 1000)
+}
+
+//com arrow function sim
+myFunc()
+function func(){
+    this.a = 'hello'
+
+    console.log(this.a)
+
+    setInterval(() => {
+        console.log(this.a)
+    }, 1000)
+}
+func()
