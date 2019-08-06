8969728285d0c1dbefa4a28d1a6aba3d36adbac5
Gabrielli Correa <gabriellicorrea7@gmail.com>
2019-08-06 11:27:13 -0300

    lista 2

---
+++ lista 02/ex1.js
@@ -0,0 +1,11 @@
+function ok(){
+    //fazer a funcao
+    
+
+}
+
+
+/*Faca uma funcao que retorne a quantidade de caracteres numericos somado a 
+quantidade de vogais de uma string recebida via parametro
+usando map/filter/reduce.*/
+
