69a481085697af1c8aceb235b3169760c0d99d2d
Gabrielli Correa <gabriellicorrea7@gmail.com>
2019-08-08 09:45:04 -0300

    teste

--- lista 02/ex1.js
+++ lista 02/ex1.js
@@ -1,9 +1,22 @@
 function ok(){
     //fazer a funcao
     
-
-
+   var str = "galera";
     
+    var filtrar = str.filter(function(pal){
+        return filtrar[pal]==='a'|| str[i]==='e'||str[i]==='i'|| str[i]==='o'|| str[i]==='u'
+
+       
+    })
+    console.log(filtrar)
+    /*
+    for(var i=0; i<=str.length; i++){
+        if(str[i]==='a'|| str[i]==='e'||str[i]==='i'|| str[i]==='o'|| str[i]==='u' || 
+        str[i]==='A'|| str[i]==='E'||str[i]==='I'|| str[i]==='O'|| str[i]==='U'){
+            c = c + 1
+        }
+    }
+*/
 }
 
 
@@ -11,3 +24,4 @@ function ok(){
 quantidade de vogais de uma string recebida via parametro
 usando map/filter/reduce.*/
 
+    
